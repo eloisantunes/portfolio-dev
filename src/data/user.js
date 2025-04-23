a185bd5fcccab26ca1c8d1a022d6973be002c82f
Eloisa Antunes <eloisantunesb@gmail.com>
2025-04-23 06:43:13 -0300

    .

---
+++ src/data/user.js
@@ -0,0 +1,85 @@
+const INFO = {
+	main: {
+		title: "portfolio by lola",
+		name: "Eloisa A.",
+		email: "eloisantunesp@gmail.com",
+		logo: "",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Full-stack web and mobile app developer — fueled by coffee, driven by curiosity.",
+		description:
+			"I am a full-stack web and mobile developer with a focus on frontend development. I have experience creating responsive, accessible, and user-friendly applications using modern frameworks and technologies. I enjoy crafting clean interfaces and bringing ideas to life through code. I am passionate about writing maintainable, well-structured code that delivers real value. I’m always looking for new challenges and opportunities to evolve and grow as a developer.",
+	},
+
+	about: {
+		title: "I’m Eloisa A. I live in São Paulo, where I design the future.",
+		description:
+			"I’ve worked on a variety of projects and I’m proud of the progress I’ve made. Many of these projects are open-source and available for others to explore and contribute. If you’re interested in any of the work I’ve done, feel free to check out the code and share any suggestions or improvements. Collaborating is a great way to learn, and I’m always open to fresh ideas and constructive feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+
+		{
+			title: "Project",
+			description:
+				"Web application for inventory management, with product registration and tracking of item entries and exits.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/typescript/typescript.png",
+			linkText: "View Project",
+			link: "https://github.com/eloisantunes/mercado-estoque",
+		},
+
+		{
+			title: "Project",
+			description:
+				"E-commerce platform built with React.js, featuring smooth navigation, a filtered product catalog...",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/eloisantunes/stec-a",
+		},
+
+		{
+			title: "Project",
+			description:
+				"Interface design project using pure CSS, with focus on responsiveness, typography, and visual hierarchy.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/css/css.png",
+			linkText: "View Project",
+			link: "https://github.com/eloisantunes/dg-joias",
+		},
+
+		{
+			title: "Project",
+			description:
+				"Music app built with JS, featuring track playback, playlist creation, and real-time audio controls. ",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/eloisantunes/music-app",
+		},
+
+		{
+			title: "Project",
+			description:
+				"Movie streaming platform with login, dynamic catalog, and trailer display. Focused on organization, security, and easy maintenance.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/php/php.png",
+			linkText: "View Project",
+			link: "https://github.com/eloisantunes/catalog-php",
+		},
+	],
+};
+
+export default INFO;
